27778ed305b93774f4ff7fa856ad4dfe98f5137d
EliasAHH <jucastillo@pcgus.com>
2025-02-19 00:07:03 -0500

    added secrets and and added logic to check if wsl or not

--- spotify_downloader.js
+++ spotify_downloader.js
@@ -1,1 +1,50 @@
 require("dotenv").config();
+const spotifyWebApi = require("spotify-web-api-node");
+const ytsr = require("ytsr");
+const { exec } = require("fs-extra");
+const path = require("path");
+const os = require("os");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirecturl: "http://localhost:8888/callback",
+});
+
+let downloadsFolder = path.join(os.homedir(), "Downloads");
+
+const isWSL = () => {
+  try {
+    return fs
+      .readFileSync("/proc/version", "utf8")
+      .toLowerCase()
+      .includes("microsoft");
+  } catch (e) {
+    return false;
+  }
+};
+
+const getWindowsHomeInWSL = () => {
+  try {
+    return execSync("cmd.exe /c echo %USERPROFILE%")
+      .toString()
+      .trim()
+      .replace(/\\/g, "/");
+  } catch (err) {
+    console.log("Error getting windows home directory:", err);
+    return null;
+  }
+};
+
+if (isWSL()) {
+  const winHome = getWindowsHomeInWSL();
+  if (winHome) {
+    downloadsFolder = path.join("/mnt/c", winHome.substring(3), "Downloads");
+  } else {
+    console.error("Failed to determine Windows home directory.");
+    process.exit(1);
+  }
+} else {
+  downloadsFolder = path.join(os.homedir(), "Downloads");
+}
